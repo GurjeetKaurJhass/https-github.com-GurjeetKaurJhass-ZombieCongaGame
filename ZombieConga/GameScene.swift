5111030f06bb792119f0df648d35056af1806122
gurjeet <gurjit.babrah@gmail.com>
2019-10-10 19:18:47 -0400

    initial commit -background setup with a new background image

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -14,6 +14,12 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         // Set the background color of the app
         self.backgroundColor = SKColor.black;
+        
+        let bg = SKSpriteNode(imageNamed: "background1")
+        bg.position = CGPoint(x: size.width/2, y: size.height/2)
+        bg.zPosition = -1
+        addChild(bg)
+        
     }
     
 }
